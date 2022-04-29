a27adbc40694c085b3c9891ec0271f6f26609aea
DuffyAdams <73561828+DuffyAdams@users.noreply.github.com>
2022-04-29 14:29:05 -0700

    Add files via upload

--- clickerProject/clicker.js
+++ clickerProject/clicker.js
@@ -36,16 +36,14 @@ var countDisplay = document.getElementById("count-display");
 var savButton = document.getElementById("saveGame");
 const progressBar = document.querySelector("#progress-bar");
 
-const darkModeToggle = document.getElementById("darkmode");
+const myBtn = document.getElementById("lightmode");
 const theme = document.getElementById("theme-link");
 
-darkModeToggle.addEventListener("click", function () {
+myBtn.addEventListener("click", function () {
   if (theme.getAttribute("href") == "styles.css") {
     theme.href = "dark-styles.css";
-    darkMode = true;
   } else {
     theme.href = "styles.css";
-    darkMode = false;
   }
 });
 
@@ -363,4 +361,29 @@ if (document.body.animate) {
   document
     .querySelectorAll("button")
     .forEach((button) => button.addEventListener("click", pop));
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
